d7cc704598335e37670f4dd54436d46c7eb3a3de
nagaoka <ngtt1212@gmail.com>
2022-02-01 22:39:45 +0900

    存在性の入力検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", address: "Example address", age: "Example age")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  test "address should be present" do
+    @user.address = "     "
+    assert_not @user.valid?
+  end
 end
