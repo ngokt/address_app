15827b320d0711f728aa275319e96415e2cc7020
nagaoka <ngtt1212@gmail.com>
2022-02-02 22:17:32 +0900

    その他の入力検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :address, presence: true, length: { maximum: 50 }
 end
