a70123f6c2b3c0ca76321ead420e543347271c14
nagaoka <ngtt1212@gmail.com>
2022-02-06 22:29:54 +0900

    jsによる年齢計算1

--- app/javascript/packs/new.js
+++ app/javascript/packs/new.js
@@ -1,3 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(document.getElementById('aiueo'));
-  });
+
+str = document.getElementById('aaa').value
+function toDate (str) {
+  var arr = (str.substr(0, 4) + '/' + str.substr(4, 2) + '/' + str.substr(6, 2)).split('/');
+  return new Date(arr[0], arr[1] - 1, arr[2]);
+};
+
+var date = toDate(str);
+
+const birthdate = date;
+ 
+const ageCalculation = ( birthDate , nowDate ) => {
+    const birthNumber = birthDate.getFullYear() * 10000 
+                               + (birthDate.getMonth() + 1 ) * 100 
+                               + birthDate.getDate();
+    const nowNumber = nowDate.getFullYear() * 10000 
+                               + (nowDate.getMonth() + 1 ) * 100 
+                               + nowDate.getDate();
+ 
+    return Math.floor( (nowNumber - birthNumber) / 10000 );
+}
+ 
+const ageage = ageCalculation( new Date(birthdate) , new Date() );
+document.getElementById('edit_area').innerHTML = ageage ;
+
+console.log( ageage + "歳" );
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 20 }
   validates :address, presence: true, length: { maximum: 50 }
+  validates :age, presence: true, length: {is: 8, message:"8桁の整数を入力してください"}
 end
